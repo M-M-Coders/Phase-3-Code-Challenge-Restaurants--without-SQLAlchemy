4d7e4df04d1e03938c9b90307d3006c114f8a307
eceechain <144310680+eceechain@users.noreply.github.com>
2023-12-03 12:08:41 +0300

    Update restaurant.py

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -1,8 +1,11 @@
+
+
 class Restaurant:
     all_restaurants = []
 
-    def __init__(self, name):
+    def __init__(self, name=""):
         self._name = name
+        self.reviews = []
         Restaurant.all_restaurants.append(self)
 
     @property
@@ -12,3 +15,16 @@ class Restaurant:
     @name.setter
     def name(self, new_name):
         self._name = new_name
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def customers(self):
+        return list(set([review.customer() for review in self.reviews]))
+
+    def average_star_rating(self):
+        if not self.reviews:
+            return 0
+        total_ratings = sum([review.rating() for review in self.reviews])
+        return total_ratings / len(self.reviews)
+
